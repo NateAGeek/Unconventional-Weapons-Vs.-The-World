98e566d1c86679c6a1ab4edda5c3d14339b89df7
HotelCalifornia <ak.brooke@sbcglobal.net>
2015-04-18 18:45:17 -0500

    cleaned up EnemyAI and removed EnemySight

--- Assets/Resources/Scripts/EnemyAI.cs
+++ Assets/Resources/Scripts/EnemyAI.cs
@@ -2,33 +2,29 @@
 using System.Collections;
 
 public class EnemyAI : MonoBehaviour {
-	
 	Vector3 waypoint;
 	Transform target;
 	NavMeshAgent agent;
 	float distance;
+	//how far away the enemy can see the player
 	float detectionrange = 50f;
+	//the range from which random waypoints can be selected
 	float range = 10f;
-	//float speed = 20f;
 
-	// Use this for initialization
 	void Start () {
+		//if this becomes multiplayer, target will become a Transform[] and we can call FindGameObjectsWithTag("Player"), and get the transforms from the ones in that list
 		target = GameObject.FindGameObjectWithTag ("Player").transform;
 		agent = GetComponent<NavMeshAgent>();
 		Wander();
 	}
-	
-	// Update is called once per frame
 	void Update () {
 		//move
 		agent.destination = waypoint;
-		//transform.position += transform.TransformDirection(Vector3.forward) * speed * Time.deltaTime;
 		
 		//have we reached the waypoint?
 		if(Vector3.Distance(transform.position, waypoint) < 3f) {
 			//go somewhere else
 			Wander();
-			//StartCoroutine("Pause");
 		}
 		if (target != null) {
 			//distance to player
@@ -39,19 +35,13 @@ public class EnemyAI : MonoBehaviour {
 			}
 		}
 	}
-
-	/*IEnumerator Pause() {
-		Wander();
-		yield return new WaitForSeconds(2);
-	}*/
-
+	//if there is no player found, the enemy will wander the area
 	void Wander() {
+		//set a new random point to go to
 		waypoint = new Vector3(Random.Range(transform.position.x - range, transform.position.x + range), 1,
 		                       Random.Range(transform.position.z - range, transform.position.z + range)
 		                      );
 		waypoint.y = 0.5f;
 		transform.LookAt(waypoint);
-		//log the waypoint and its distance
-		Debug.Log(waypoint + " and " + (transform.position - waypoint).magnitude);
 	}
 }

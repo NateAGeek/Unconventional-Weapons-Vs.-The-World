a74a60f753d4713c7e72cb3ca1e785faf2766406
HotelCalifornia <ak.brooke@sbcglobal.net>
2015-04-18 22:51:46 -0500

    refactored EnemyAI into a helper script. Added HealthManager abstract class. Added Enemy as the script that the enemy prefab will use

---
+++ Assets/Resources/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : HealthManager {
+
+	private NavMeshAgent agent;
+	private EnemyAI ai;
+
+	private CapsuleCollider damager;
+
+	// Use this for initialization
+	void Start () {
+		this.maxHealth = 100;
+		this.currentHealth = this.maxHealth;
+		this.agent = GetComponent<NavMeshAgent> ();
+		this.ai = new EnemyAI (this.agent);
+		this.damager = GetComponent<CapsuleCollider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ai.Update (this.transform);
+	}
+
+	public override void OnDead() {
+		//handle death
+		Destroy (this.gameObject);
+	}
+}
---
+++ Assets/Resources/Scripts/HealthManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class HealthManager : MonoBehaviour {
+	public int maxHealth;
+	public int currentHealth;
+	public bool isAlive = true;
+
+	/// <summary>
+	/// when one entity damages another, call this method to apply the damage.
+	/// for example, if a gnome hits the player, the gnome would call:
+	/// player.ReceiveDamage(damageValue);
+	/// </summary>
+	/// <param name="damage">Damage dealt</param>
+	public void ReceiveDamage(int damage) {
+		this.currentHealth -= damage;
+
+		if (this.currentHealth <= 0) {
+			this.isAlive = false;
+		}
+	}
+
+	/// <summary>
+	/// called when isAlive = false, preferably in the Update method
+	/// handles death of an entity
+	/// </summary>
+	public abstract void OnDead();
+}

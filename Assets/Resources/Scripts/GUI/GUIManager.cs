cde2e078a84f7f7561902458413e49d413658595
Kevin Ye <kevin.ye204@gmail.com>
2015-04-19 00:45:22 -0500

    GUIManager added

---
+++ Assets/Resources/Scripts/GUI/GUIManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour {
+	public Transform inventoryMenu;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Open Inventory")){
+			inventoryMenu.gameObject.SetActive(!inventoryMenu.gameObject.activeSelf);
+		}
+	}
+}

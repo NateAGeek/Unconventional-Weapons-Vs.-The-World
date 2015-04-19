aeb5fe72e5429b8253304fc577992e6973cfb589
Kevin Ye <kevin.ye204@gmail.com>
2015-04-18 19:10:34 -0500

    Pick up scraps, attack

--- Assets/Resources/Scripts/Weapons/Weapon.cs
+++ Assets/Resources/Scripts/Weapons/Weapon.cs
@@ -11,11 +11,33 @@ public class Weapon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetButtonDown("Fire1")){
+			Debug.Log("Firin' mah lazer");
+		}
+		if(Input.GetButtonDown("Interact")){
+			RaycastHit hit;
+			Ray ray = transform.parent.gameObject.GetComponentInChildren<Camera>().ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2));
+
+			if(Physics.Raycast(ray, out hit)){
+				if(hit.transform.gameObject.tag == "Interactable"){
+					outfit = hit.transform.gameObject.GetComponent<Scrap>();
+					Rigidbody itembody = hit.transform.gameObject.GetComponent<Rigidbody>();
+					itembody.isKinematic = true;
+					hit.transform.rotation = transform.rotation;
+					hit.transform.position = transform.position;
+					hit.transform.SetParent(transform, true);
+				}
+			}
+		}
 	}
 
 	public void equipItem(IWeaponOutfit o)
 	{
 		outfit = o;
+	}
+
+	public bool itemEquipped()
+	{
+		return outfit != null;
 	}
 }
